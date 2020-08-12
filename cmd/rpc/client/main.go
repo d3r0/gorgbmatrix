47c06c3e6e72a12b08d3c4311382d3cab2fcc934
d3r0 <marcel.arndt@hotmail.de>
2020-08-12 09:27:07 +0200

    init

---
+++ cmd/rpc/client/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/d3r0/gorgbmatrix/rpc"
+)
+
+var (
+	img = flag.String("image", "", "image path")
+)
+
+func main() {
+	f, err := os.Open(*img)
+	fatal(err)
+
+	m, err := rpc.NewClient("tcp", "10.20.20.20:1234")
+	fatal(err)
+
+	tk := rgbmatrix.NewToolKit(m)
+	close, err := tk.PlayGIF(f)
+	fatal(err)
+
+	time.Sleep(time.Second * 3)
+	close <- true
+}
+
+func init() {
+	flag.Parse()
+}
+
+func fatal(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/d3r0/gorgbmatrix
+
+go 1.13
+
+require (
+	github.com/disintegration/imaging v1.6.2
+	github.com/fogleman/gg v1.3.0
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/exp v0.0.0-20200513190911-00229845015e
+	golang.org/x/mobile v0.0.0-20200801112145-973feb4309de
+)
